c14c86ce6b7b07331f60a1855550141c207e6f98
Sazied Hassan <mshassan@mun.ca>
2020-09-11 14:43:25 -0230

    authentication

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-const Joi = require('joi'); 
-Joi.objectId = require('joi-objectid')(Joi); 
+const Joi = require("joi");
+Joi.objectId = require("joi-objectid")(Joi);
+const config = require("config");
 const mongoose = require("mongoose");
 const genres = require("./routes/genres");
 const customers = require("./routes/customers");
-const rentals = require('./routes/rentals'); 
-const movies = require('./routes/movies'); 
-const users = require('./routes/users');
+const rentals = require("./routes/rentals");
+const movies = require("./routes/movies");
+const users = require("./routes/users");
+const auth = require("./routes/auth");
 const express = require("express");
 const app = express();
+
+if (!config.get("jwtPrivateKey")) {
+  console.error("FATAL ERROR: jwtPrivateKey is not defined.");
+  process.exit(1);
+}
 
 mongoose
   .connect("mongodb://localhost/VIDLY")
@@ -17,9 +24,10 @@ mongoose
 app.use(express.json());
 app.use("/api/genres", genres);
 app.use("/api/customers", customers);
-app.use('/api/movies', movies); 
-app.use('/api/rentals', rentals); 
-app.use('/api/users', users); 
+app.use("/api/movies", movies);
+app.use("/api/rentals", rentals);
+app.use("/api/users", users);
+app.use("/api/auth", auth);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`)); // Listening to port dynamically
---
+++ routes/auth.js
@@ -0,0 +1,40 @@
+const _ = require("lodash");
+const bcrypt = require('bcrypt'); 
+const jwt = require('jsonwebtoken'); 
+const config = require('config'); 
+const { User } = require("../models/user");
+const Joi = require('joi'); 
+const mongoose = require("mongoose");
+const express = require("express");
+const router = express.Router();
+
+// CRUD OPERATIONS AND END-POINTS
+
+// GET
+
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Invalid email or password.");
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password); 
+  if (!validPassword) return res.status(400).send('Invalid email or password'); 
+
+  const token = jwt.sign({ _id: user._id }, config.get('jwtPrivateKey')); 
+
+  res.send(token); 
+});
+
+const validate = (req) => {
+    const schema = Joi.object({
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(10).max(255).required(),
+    });
+
+    return schema.validate(req); 
+}; 
+
+
+module.exports = router;
